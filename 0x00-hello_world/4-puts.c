95c083f63f5d1cf86f074d738f723df85cece13e
Machiii <blesseduko17@gmail.com>
2023-03-06 03:36:59 -0800

    Programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 /** 
  * main - display to stdio
- * Return 0 if code runs
+ * Return 0 if code is effective
  */
 
 int (main)void
 {
-puts("\"Programming is like building a multilingual puzzle);
+puts("/"Programming is like building a multilingual puzzle);
 }
